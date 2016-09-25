966d0ee24297d0b7347d53786ddd4374027a690d
Vovan205 <vovan305@yandex.ru>
2016-09-25 14:51:01 +0300

    Update arr_algs.py

--- arr_algs.py
+++ arr_algs.py
@@ -1,9 +1,9 @@
-def min(arr1):
-    minimal = arr1[0]
-    for i, el in enumerate(arr1):
-        if el < minimal:
-            minimal = el
+def min(arr1):  #минимальное значение
+    minimal = arr1[0] #принимаем нулевой элемент массива за минимальный
+    for i, el in enumerate(arr1): #для каждого элемента в массиве
+        if el < minimal: #если элемент меньше минимального 
+            minimal = el #то обновляем минимальное значение
     return minimal
 arr1 = [8, 5, 6, 9, 2, 1, 78]
 print(min(arr1))
-print(sum(arr1) / len(arr1))
+print(sum(arr1) / len(arr1)) #среднее значение

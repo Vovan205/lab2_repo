f3fe72cbdb11276c5b1344f67c1b0509280a957e
Vovan205 <vovan305@yandex.ru>
2016-09-25 14:40:36 +0300

    Delete arr_algs.py

--- arr_algs.py
+++
@@ -1,9 +0,0 @@
-def min(arr1):
-    minimal = arr1[0]
-    for i, el in enumerate(arr1):
-        if el < minimal:
-            minimal = el
-    return minimal
-arr1 = [8, 5, 6, 9, 2, 1, 78]
-print(min(arr1))
-
